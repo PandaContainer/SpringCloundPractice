21c331d7f8680db127a877b313088f308d666863
xuhongbao <xuhongbao1@163.com>
2020-02-24 22:15:03 +0800

    Turbine基于消息代理RabbitMQ搜集实现

--- TurbineAmqp/src/main/java/com/test/springcloud/turbineamqp/TurbineAmqpApplication.java
+++ TurbineAmqp/src/main/java/com/test/springcloud/turbineamqp/TurbineAmqpApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
+/**
+ * Turbine基于消息代理RabbitMQ搜集实现<br>
+  *  使用新版本hystrix功能的服务消费者引入spring-cloud-netflix-hystrix-stream依赖?
+ * @author xuhon
+ *
+ */
 @EnableTurbineStream
 @EnableDiscoveryClient
 @SpringBootApplication
